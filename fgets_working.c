2ef8d05e3ab1eba9d669caf01b209ba3deff2da7
Karthik Ragunath Ananda Kumar <kxa200005@csjaws.utdallas.edu>
2021-09-21 21:38:41 -0500

    checking working of fgets

---
+++ fgets_working.c
@@ -0,0 +1,30 @@
+#include<stdlib.h>
+#include<math.h>
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main()
+{
+	char* line = (char *)malloc(50 * sizeof(char));
+	int len = 50;
+	char* read;
+	ssize_t read_getline;
+	
+	while(fgets(line, len, stdin) != NULL)
+	{
+		printf("The string len via fgets is %d\n", strlen(line));
+		break;
+	}
+	
+	line = NULL;
+	size_t len_getline = 0;
+	
+	while((read_getline = getline(&line, &len_getline, stdin)) != -1)
+	{
+		printf("The string len is %d\n", strlen(line));
+		break;
+	}
+	return 0;
+}
