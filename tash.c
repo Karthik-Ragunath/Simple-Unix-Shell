3b5689ecd5f74824e09178822f668616cca979d0
Karthik Ragunath Ananda Kumar <a.karthikragunath@gmail.com>
2021-11-09 01:32:05 -0600

    spelling typo

--- tash.c
+++ tash.c
@@ -7,7 +7,7 @@
 
 void print_error()
 {
-	char error_message[30] = "An error has occured\n";
+	char error_message[30] = "An error has occurred\n";
 	write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
